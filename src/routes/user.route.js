1dbabff0ddd3d9d4be3b15db7a82a5495209f92c
Sinan Aksu <sinanksu@gmail.com>
2021-12-05 01:36:41 +0300

    feat: add me method to user.route.js

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,7 @@
 var router = require("express").Router();
 const user = require("../controllers/user.controller");
+const { verifyToken } = require("../middlewares/auth");
+
 const {
   isEmail,
   isUsername,
@@ -7,6 +9,7 @@ const {
 } = require("../middlewares/validation");
 
 module.exports = (app) => {
+  router.get("/me", verifyToken, user.me);
   router.post("/login", isEmail, isPassword, user.login);
   router.post("/register", isEmail, isUsername, isPassword, user.register);
   app.use("/v1/user", router);
