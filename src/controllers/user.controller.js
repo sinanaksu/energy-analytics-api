872a5a37aba2cc3bac5896462aebba748d1fbd54
Sinan Aksu <sinanksu@gmail.com>
2021-12-05 01:35:12 +0300

    feat: add login to user.controller.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,9 @@
 const User = require("../models/user.model");
 var bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("../utils/config");
 
+// User Register Controller
 exports.register = async (req, res) => {
   const data = req.body;
   data.password = bcrypt.hashSync(data.password, 8);
@@ -13,4 +16,43 @@ exports.register = async (req, res) => {
     .catch((err) => {
       res.status(500).send(err);
     });
+};
+
+// User Login Controller
+exports.login = async (req, res) => {
+  await User.findOne({
+    email: req.body.email,
+  }).exec((err, user) => {
+    if (err) {
+      res.status(500).send({ error: err });
+      return;
+    }
+
+    if (!user) {
+      return res.status(404).send({ error: "USER_NOT_FOUND" });
+    }
+
+    const passwordIsValid = bcrypt.compareSync(
+      req.body.password,
+      user.password
+    );
+
+    if (!passwordIsValid) {
+      return res.status(401).send({
+        accessToken: null,
+        error: "USER_NOT_FOUND",
+      });
+    }
+
+    const token = jwt.sign({ id: user._id }, config.SECRET, {
+      expiresIn: 86400, // 24 hours
+    });
+
+    res.status(200).send({
+      id: user._id,
+      username: user.name,
+      email: user.email,
+      accessToken: token,
+    });
+  });
 };
