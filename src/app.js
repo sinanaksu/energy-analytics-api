ba24ce278a8cda66eb051920f185d5eb34bc8fb8
Sinan Aksu <sinanksu@gmail.com>
2021-12-04 03:36:33 +0300

    perf: minimized app.js

--- src/app.js
+++ src/app.js
@@ -1,25 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const methodOverride = require("method-override");
 const cors = require("cors");
-const dotenv = require("dotenv");
 const app = express();
 
-dotenv.config();
-
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(methodOverride());
 
-app.get("/user", (req, res) => {
-  res.json(200);
-});
-
-const server = app.listen(5000, () => {
-  console.log(
-    "Server has started... Port: http://localhost:%d",
-    server.address().port
-  );
-});
+const server = app.listen(5000);
 
 module.exports = server;
