9381d14283435d19b3f9fc50011d7a1c5d778750
Sinan Aksu <sinanksu@gmail.com>
2021-12-04 03:30:37 +0300

    chore: change eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,13 @@
 module.exports = {
     "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2021": true
+        "node": true,
+        "commonjs": true
     },
     "extends": "eslint:recommended",
     "parserOptions": {
-        "ecmaVersion": 13
+        "ecmaVersion": 11,
+        "sourceType": "module",
+        "allowImportExportEverywhere": true
     },
     "rules": {
     }
