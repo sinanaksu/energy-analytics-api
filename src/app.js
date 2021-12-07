a0e72b5137674f6849b418bde7d0b831adf3656d
Sinan Aksu <sinanksu@gmail.com>
2021-12-07 04:46:30 +0300

    feat: add facility route to app.js

--- src/app.js
+++ src/app.js
@@ -4,11 +4,13 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 require("./utils/mongodb");
 require("./routes/user.route")(app);
+require("./routes/facility.route")(app);
 
 const server = app.listen(5000);
 
