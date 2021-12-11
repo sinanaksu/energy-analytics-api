14213b1fd68f28651eb5f0bce296235df81b59ab
Sinan Aksu <sinanksu@gmail.com>
2021-12-11 13:57:21 +0300

    feat: add facility usage routes

--- src/routes/facility.route.js
+++ src/routes/facility.route.js
@@ -6,5 +6,12 @@ module.exports = (app) => {
   router.get("/", verifyToken, facility.list);
   router.put("/", verifyToken, facility.update);
   router.post("/", verifyToken, facility.create);
+  router.delete("/", verifyToken, facility.delete);
+
+  router.post("/usage/list", verifyToken, facility.listUsage);
+  router.put("/usage/", verifyToken, facility.updateUsage);
+  router.post("/usage/", verifyToken, facility.createUsage);
+  router.delete("/usage/", verifyToken, facility.deleteUsage);
+
   app.use("/v1/facility", router);
 };
