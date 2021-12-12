54f697bd50970aad5ef8d9a908928f343341e941
Sinan Aksu <sinanksu@gmail.com>
2021-12-12 10:49:36 +0300

    feat: add heatmap route

--- src/routes/analytics.route.js
+++ src/routes/analytics.route.js
@@ -4,6 +4,6 @@ const { verifyToken } = require("../middlewares/auth");
 
 module.exports = (app) => {
   router.get("/day/", verifyToken, analytics.usage);
-
+  router.get("/heatmap/", analytics.heatmap);
   app.use("/v1/analytics", router);
 };
