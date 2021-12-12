9fea663b6672139d1b2a97b652061641aece826f
Sinan Aksu <sinanksu@gmail.com>
2021-12-12 12:52:01 +0300

    feat: add series/phases route

--- src/routes/analytics.route.js
+++ src/routes/analytics.route.js
@@ -5,5 +5,6 @@ const { verifyToken } = require("../middlewares/auth");
 module.exports = (app) => {
   router.get("/day/", verifyToken, analytics.usage);
   router.get("/heatmap/", analytics.heatmap);
+  router.get("/series/phases/", analytics.phases);
   app.use("/v1/analytics", router);
 };
